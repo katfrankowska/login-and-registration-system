a50f71e41849a59baaca34dbb296daf3bbef2f46
katfrankowska <66004567+katfrankowska@users.noreply.github.com>
2021-09-07 12:04:45 +0200

    Add project files.

---
+++ frmLogin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Login_and_registration_system
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users.mdb");
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da = new OleDbDataAdapter();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string login = "SELECT * FROM tbl_user WHERE username='" + txtUsername.Text + "' and password='" + txtPassword.Text + "'";
+            cmd = new OleDbCommand(login, con);
+            OleDbDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read() == true)
+            {
+                new dashboard().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password, Please try again", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                txtUsername.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtUsername.Focus();
+        }
+
+        private void checkbxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbxShowPas.Checked)
+            {
+                txtPassword.PasswordChar = '\0';
+                
+            }
+            else
+            {
+                txtPassword.PasswordChar = '•';
+                
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            new frmRegister().Show();
+            this.Hide();
+        }
+    }
+}
